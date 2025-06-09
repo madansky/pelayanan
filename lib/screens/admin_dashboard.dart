1033845c1551ee6c4a54a104d483e56483b7d0d4
madansky <madanskytrash@gmail.com>
2025-06-09 19:13:53 +0900

    Create admin_dashboard.dart

---
+++ lib/screens/admin_dashboard.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AdminDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard Admin')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('reports')
+            .orderBy('timestamp', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+
+          final docs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final data = docs[index];
+              return ListTile(
+                title: Text(data['name']),
+                subtitle: Text(data['report']),
+                trailing: Text(data['email']),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
